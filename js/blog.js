c1657c74c934e2a0b64cdd6d12a87daf5c2852ef
imjoyhalder <joyhalder00113355@gamil.com>
2025-03-11 14:13:57 +0600

    fix blog-btn issue

--- js/blog.js
+++ js/blog.js
@@ -1,11 +1,20 @@
 
+// document.getElementById('blog-btn')
+//     .addEventListener('click', function() {
+//         window.location.href = '/blog.html';  // Corrected method
+//     });
+
+// document.getElementById('home-btn')
+//     .addEventListener('click', function() {
+//         window.location.href = '/index.html';
+// });
+    
 document.getElementById('blog-btn')
     .addEventListener('click', function() {
-        window.location.href = '/blog.html';  // Corrected method
+        window.location.href = window.location.origin + '/Donation-System/blog.html';
     });
 
 document.getElementById('home-btn')
     .addEventListener('click', function() {
-        window.location.href = '/index.html';
-});
-    
+        window.location.href = window.location.origin + '/Donation-System/index.html';
+    });
